8b42cad2d7e26edde58a084663d4262089fefd06
pradyuman shukla <shuklapradyuman786@gmail.com>
2022-11-21 20:08:13 +0530

    D

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -69,37 +69,7 @@ const Projects = () => {
       style: "hover:text-blue-400 duration-200",
       disc: "This is a simple food searching app, this app will shows the foods whichever you search. I have buit this app using HTML, CSS, and JavaScript. Api used to fetch the data and rendering on DOM",
       techStack: ["HTML", "CSS", "JS", "API", "NodeJS"],
-    },
-    // {
-    //   id: 6,
-    //   title: "React Movie-Searching App",
-    //   src: reactMovieapp,
-    //   link: "https://moive-app-gowtham-chokkalingam.vercel.app/",
-    //   git: "https://github.com/Gowtham-Chokkalingam/react-projects/tree/master/moive-app",
-    //   style: "hover:text-blue-400 duration-200",
-    //   disc: "This is Movie App built by using React, SCSS and React-Redux-Toolkit. This app will give you the information about Movies which we search and also displays the trending moives by default.",
-    //   techStack: ["React", "SCSS", "Redux-Toolkit", "Axios-API"],
-    // },
-    // {
-    //   id: 5,
-    //   title: "Weather App",
-    //   src: weather,
-    //   link: "https://incredible-frangollo-40a170.netlify.app/",
-    //   git: "https://github.com/Gowtham-Chokkalingam/WebDev-Projects/tree/main/FInalFoodApp/Food-App-III",
-    //   style: "hover:text-blue-400 duration-200",
-    //   disc: "This is a Weather App made by using HTML, CSS and Javascript. This app will give you the information about weather for current day and for next three days also. This app also allow you to see your city or location on google map",
-    //   techStack: ["HTML", "CSS", "JS", "Axios-API", "NodeJS"],
-    // },
-    // {
-    //   id: 1,
-    //   title: "Travelocity",
-    //   src: travellocity,
-    //   link: "https://meek-blancmange-7c0dba.netlify.app/",
-    //   git: "https://github.com/Code-With-Hashim/boss-magic-6284",
-    //   style: "hover:text-blue-400 duration-200",
-    //   disc: "This was a group project. We have created this website with the help of HTML, CSS And Javascript. My work was here to create Navbar, SignUp, Login, Logout part.",
-    //   techStack: ["HTML", "CSS", "JS", "NodeJS"],
-    // },
+    }
   ];
   const [loading, setLoading] = useState(true);
   useEffect(() => {
